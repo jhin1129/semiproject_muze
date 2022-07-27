e99d20e09bcbd04894d19c76ffd383474440281b
DESKTOP-J0V7617\ADMIN <yerinlee.yr@gmail.com>
2022-07-27 09:59:15 +0900

    캘린더 DB 작업중

--- muze/src/main/java/com/muze/mvc/event/model/dao/EventDao.java
+++ muze/src/main/java/com/muze/mvc/event/model/dao/EventDao.java
@@ -1,5 +1,31 @@
 package com.muze.mvc.event.model.dao;
+import static com.muze.mvc.common.jdbc.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.muze.mvc.event.model.vo.Mileage;
 
 public class EventDao {
-
+	
+		public int insertAttMileage (Connection connection, Mileage mileage) {
+			int result = 0;
+			PreparedStatement pstmt = null;
+			String query = "INSERT INTO MILEAGE VALUES(SEQ_POINT,?,100,'ATT',DEFAULT,'IN')";
+			
+			try {
+				pstmt = connection.prepareStatement(query);
+				
+				pstmt.setInt(1, mileage.getMemberNo());
+				
+				result = pstmt.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				close(pstmt);
+			}
+			
+			return result;
+		}
 }
--- muze/src/main/webapp/views/event/eventpage.jsp
+++ muze/src/main/webapp/views/event/eventpage.jsp
@@ -29,6 +29,7 @@
         <div class="container calendar-container">
           
           <script>
+         	var calendar = null;
             $(document).ready(function() {
             var calendarEl = document.getElementById('calendar');
             var calendar = new FullCalendar.Calendar(calendarEl, {
