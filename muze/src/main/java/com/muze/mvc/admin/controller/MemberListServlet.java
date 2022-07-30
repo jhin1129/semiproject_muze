c9923ba7436ed26c4c773f06c7e341dedce76ddb
dschoi30 <ds0631@gmail.com>
2022-07-30 10:58:21 +0900

    220730 관리자 페이지 재커밋

---
+++ muze/src/main/java/com/muze/mvc/admin/controller/MemberListServlet.java
@@ -0,0 +1,48 @@
+package com.muze.mvc.admin.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.muze.mvc.admin.model.service.AdminService;
+import com.muze.mvc.common.util.PageInfo;
+import com.muze.mvc.member.vo.Member;
+
+@WebServlet("/admin/member")
+public class MemberListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MemberListServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int page = 0;
+		int listCount = 0;
+		PageInfo pageInfo = null;
+		List<Member> list = null;
+		
+		try {
+		page = Integer.parseInt(request.getParameter("page"));
+		} catch(NumberFormatException e) {
+			page = 1;
+		}
+		
+		listCount = new AdminService().getMemberCount();
+		pageInfo = new PageInfo(page, 2, listCount, 10);
+		list = new AdminService().getMemberList(pageInfo);
+		
+		request.setAttribute("pageInfo", pageInfo);
+		request.setAttribute("list", list);
+
+		System.out.println(listCount);
+		System.out.println(list);
+		
+		request.getRequestDispatcher("/views/adminpage/members.jsp").forward(request, response);
+	}
+
+}
